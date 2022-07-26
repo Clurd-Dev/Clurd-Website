a6a2014a6bc9c42fc4714c03133d00abf3b09eb3
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-07-26 10:29:48 +0200

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { onMount } from 'svelte';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    onMount(async() => {
+        const module = await import('bootstrap/dist/js/bootstrap.bundle.js');
+    });
+</script>
+
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Clurd.NET</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Getting started</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Download</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Source code</a>
+          </li>
+        </ul>
+    
+      </div>
+    </div>
+  </nav>
+  <br/>
+  <br/>
+<main>
+   <slot>
+
+</slot> 
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,104 @@
+<script>
+
+</script>
+
+
+
+
+<div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <center>
+            <img src="/favicon.png" alt="logo"/>
+            <h1>Clurd.NET</h1>
+            <h4>A self hosting cloud application light and fast</h4>
+        </center>
+      </div>
+      <div class="col">
+        
+      </div>
+      <div class="col">
+        Column
+      </div>
+    </div>
+  </div>
+
+  <br/>
+  <br/>
+
+<div class="card m-5">
+    <div class="card-body">
+      <h5 class="card-title" style="">Cross-Platform</h5>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <p class="card-text">Clurd is completely Cross-Platform thanks to the backend written in .NET Core and the client applications written with Flutter.
+            </p>
+            <p class="card-text">You can install Clurd server in your Windows, Mac OS or Linux machine to serve and upload files then you can do any operation, such copy, move, download, upload... in the Clurd-Client, the application to manage Clurd server</p>
+          </div>
+          <div class="col">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <a title="Jason Groce, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:.NET_Core_Logo.svg"><img width="256" alt=".NET Core Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/512px-.NET_Core_Logo.svg.png"></a>
+                    </div>
+                    <div class="col">
+                        <img width="200" src="flutter.svg" alt="Flutter logo"/>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+<!--  <Card class="m-5">
+    <CardHeader>
+        <CardTitle>
+            
+        </CardTitle>
+    </CardHeader>
+    <CardBody>
+        <Row>
+            <Col>
+                
+            </Col>
+            <Col>
+                <Row>
+                    <Col>
+
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    </CardBody>
+  </Card>
+
+  <Card class="m-5">
+    <CardHeader>
+        <CardTitle>
+            Light and Fast
+        </CardTitle>
+    </CardHeader>
+    <CardBody>
+        <Row>
+            <Col>
+                <p>Clurd can run everywhere, it use &gt 100Mb of ram and &gt 10% of CPU in the main thread. You can use in every architecture such Raspberry Pi and other ARM platform.</p>
+            </Col>
+             <Col>
+                <Row>
+                    <Col>
+                        <a title="Jason Groce, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:.NET_Core_Logo.svg"><img width="256" alt=".NET Core Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/512px-.NET_Core_Logo.svg.png"></a>
+                    </Col>
+                    <Col>
+                        <img width="200" src="flutter.svg" alt="Flutter logo"/>
+                    </Col>
+                </Row>
+            </Col> -->
+        <!--</Row>
+    </CardBody>
+  </Card>
+
+ -->
